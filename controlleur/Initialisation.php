8e5b0ab803f9d7ee86b7cb7969d031f1c89d9ab9
Lucas Millet <lulumlt29@gmail.com>
2017-04-26 21:46:42 +0200

    Test 3 (on récupère la question en on envoie à l'ihm, les echo sont juste là pour le test)

--- controlleur/Initialisation.php
+++ controlleur/Initialisation.php
@@ -13,8 +13,20 @@ $nomJoueur = $unJoueur->getPrenom();
 
 $Question = $Liste->GetUneQuestion(0);
 
+$laQuestion = $Question->GetQuestion();
+$Reponse = $Question->GetReponse();
+$Erreur1 = $Question->GetErreur1();
+$Erreur2 = $Question->GetErreur2();
+$Erreur3 = $Question->GetErreur3();
+
+//Echo pour test
+echo  $laQuestion."<br/>";
+echo  $Reponse."<br/>";
+echo  $Erreur1."<br/>";
+echo  $Erreur2."<br/>";
+echo  $Erreur3."<br/>";
 
 //retourne question1
-header("Location : ..\vue\question.php?Nom=$nomJoueur&Question=$Question");
+header("Location : ..\vue\question.php?Nom=$nomJoueur&Question=$laQuestion&Reponse=$Reponse&Erreur1=$Erreur1&Erreur2=$Erreur2&Erreur3=$Erreur3");
 exit();
 ?>
