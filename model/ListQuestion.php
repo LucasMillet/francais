c9b772b5d30455f1fb8aed0bb3916dee255ca968
Lucas Millet <lulumlt29@gmail.com>
2017-04-30 12:40:56 +0200

    Gestion des accents

--- model/ListQuestion.php
+++ model/ListQuestion.php
@@ -9,15 +9,15 @@ class ListQuestion {
 	////////////////////  GET ans setter ///////
 	public function ListQuestionPopulated(){
 		
-		$this->LesQuestions[0] = new Question("Combien faut-il faire de pages en synthese ?", "4-5 Pages" ,"3-4 Pages", "2-3 Pages" , "5-6 Pages") ;
-		$this->LesQuestions[1] = new Question("Peut-on faire des citations dans la synthese ?","Non, il ne faut pas","Oui, on peut le faire si l'on veut","Oui, c est obligatoire ", "Ca depend de quelle citation") ;
-		$this->LesQuestions[2] = new Question("Quel pronom personnel doit-on utiliser majoritairement lors de la synthese ?","On, Nous","Je","Toi, Tu","Vous") ;
-		$this->LesQuestions[3] = new Question("Ou doivent etre situe les presentations des textes ?","Dans le developpement avant leur utilisation","Dans l introduction","Dans le developpement apres leur utilisation", "Dans la conclusion") ;
-		$this->LesQuestions[4] = new Question("A quoi sert la conclusion ?","Elle doit etre le reflet du plan et synthetiser le developpement","Elle doit repondre a la problematique","Elle synthetise le plan et est le reflet de l'introduction", "Elle permet une ouverture") ;
-		$this->LesQuestions[5] = new Question("Dans l'introduction, dans quel ordre ces elements doivent ils etre places ?","Amorce, problematique, plan","Plan, problematique, amorce","Amorce, plan, problematique", "Problematique, plan, amorce") ;
-		$this->LesQuestions[6] = new Question("Est ce qu une ouverture dans la conclusion est souhaitable","Non elle est deconseillee","Oui elle est souhaitable","Oui, elle est meme obligatoire","Non elle n'est pas obligatoire") ;
-		$this->LesQuestions[7] = new Question("Que faut il faire sur les titres des oeuvres?","Il faut les souligner","Il faut les entourer de guillemets","Il faut les ecrire en italique","Rien de special") ;
-		$this->LesQuestions[8] = new Question("Quel est le but d'une synthese ?","Confronter les idees des documents et les resumer","Resumer les documents de maniere synthetique","Presenter les documents et en degager les idees","Regrouper les documents en themes communs") ;
+		$this->LesQuestions[0] = new Question("Combien faut-il faire de pages en synthèse ?", "4-5 Pages" ,"3-4 Pages", "2-3 Pages" , "5-6 Pages") ;
+		$this->LesQuestions[1] = new Question("Peut-on faire des citations dans la synthèse ?","Non, il ne faut pas","Oui, on peut le faire si l&#146;on veut","Oui, c est obligatoire ", "Ca dépend de quelle citation") ;
+		$this->LesQuestions[2] = new Question("Quel pronom personnel doit-on utiliser majoritairement lors de la synthèse ?","On, Nous","Je","Toi, Tu","Vous") ;
+		$this->LesQuestions[3] = new Question("Ou doivent être situé les présentations des textes ?","Dans le développement avant leur utilisation","Dans l&#146;introduction","Dans le développement après leur utilisation", "Dans la conclusion") ;
+		$this->LesQuestions[4] = new Question("A quoi sert la conclusion ?","Elle doit être le reflet du plan et synthetiser le développement","Elle doit répondre à la problématique","Elle synthetise le plan et est le reflet de l'introduction", "Elle permet une ouverture") ;
+		$this->LesQuestions[5] = new Question("Dans l&#146;introduction, dans quel ordre ces éléments doivent ils être placés ?","Amorce, problématique, plan","Plan, problématique, amorce","Amorce, plan, problématique", "Problématique, plan, amorce") ;
+		$this->LesQuestions[6] = new Question("Est ce qu&#146;une ouverture dans la conclusion est souhaitable","Non elle est deconseillée","Oui elle est souhaitable","Oui, elle est même obligatoire","Non elle n&#146;est pas obligatoire") ;
+		$this->LesQuestions[7] = new Question("Que faut il faire sur les titres des oeuvres?","Il faut les souligner","Il faut les entourer de guillemets","Il faut les écrire en italique","Rien de spécial") ;
+		$this->LesQuestions[8] = new Question("Quel est le but d&#146;une synthèse ?","Confronter les idées des documents et les résumer","Résumer les documents de manière synthetique","Présenter les documents et en dégager les idées","Regrouper les documents en thèmes communs") ;
 	}
 	
 	public function getLesQuestions() {
--- vue/question.php
+++ vue/question.php
@@ -3,10 +3,11 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
  <head>
-    <meta charset="utf-8">
+    
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta name="description" content="">
     <meta name="author" content="">
 
@@ -53,7 +54,6 @@ $proposition4 = $ListeProposition[3];
 
 
 $vieActuelle = $_POST['Vie'];
-
 
 //// test ////
 
